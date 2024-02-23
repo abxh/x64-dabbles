e0b6373c2e535f7722b52a9923756fb829ba5f10
abxh <83485102+abxh@users.noreply.github.com>
2024-02-24 00:29:30 +0100

    Update npow.asm

--- 05-recursion/npow.asm
+++ 05-recursion/npow.asm
@@ -40,8 +40,8 @@ section .text
             dec exponent            ; exponent <- exponent - 1
             shr exponent, 1         ; exponent <- exponent >> 1
 
-            mov rdi, base           ; rdi <- base
-            mov rsi, exponent       ; rsi <- exponent
+            mov rdi, base           ; rdi (param1) <- base
+            mov rsi, exponent       ; rsi (param2) <- exponent
             call npow               ; rax <- npow(base, exponent)
 
             mul rax                 ; rax <- rax * rax
